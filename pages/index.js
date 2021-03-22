c8f6a1aef3566f72c315a809d56e5bbce4e36de4
Topher <christopher.w.liu@gmail.com>
2021-03-22 13:22:54 -0800

    added boilerplate from next js and added inupiaq welcome text

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+
+  var greetingInupiaq = 'Paġlagikpiñ'
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          {greetingInupiaq}
+        </h1>
+        <input placeholder="Type here..." style={{margin:20}}/>
+       
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
