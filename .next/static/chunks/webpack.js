f72b0b289a6f42a70ac5115cbadc0772c728c50e
Topher <christopher.w.liu@gmail.com>
2021-04-15 22:51:23 -0800

    added andru

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e66f69a663d6bc46be24";
+/******/ 	var hotCurrentHash = "41fd40b0c654f303a52e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
